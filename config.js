33b731a35b7720b20267557373f931c3dd1ee2b2
Miguel Perez <miguellrvc@gmail.com>
2023-01-23 21:48:40 -0300

    modificación del archivo config.js

--- config.js
+++ config.js
@@ -1,13 +1,9 @@
-const PORT = 3000;
-// const HOST = process.env.HOST || "localhost";
-// const DATABASE = process.env.DATABASE || "porfolio" ;
-// const USER = process.env.USER || "root";
-// const PASSWORD = process.env.PASSWORD || "";
+const PORT = process.env.PORT || 3000;
+const HOST = process.env.HOST || "localhost";
+const DATABASE = process.env.DATABASE || "porfolio" ;
+const USER = process.env.USER || "root";
+const PASSWORD = process.env.PASSWORD || "";
 
-const HOST = process.env.HOST || "sql726.main-hosting.eu";
-const DATABASE = process.env.DATABASE || "u198200869_porfolio" ;
-const USER = process.env.USER || "u198200869_miguellrvc";
-const PASSWORD = process.env.PASSWORD || "!M@ReX2v";
 
 module.exports = {
     PORT,
